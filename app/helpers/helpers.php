7645c2343e982b458182cfb54a25bfc2d6752bf1
abubakkar360 <mdabubakkar014041@gmail.com>
2024-12-09 13:17:59 +0600

    first commit

---
+++ app/helpers/helpers.php
@@ -0,0 +1,15 @@
+<?php 
+
+function view($fileName, $data = [],$print = true){
+    $output = "view Not Found";
+    $filePath = "views/".$fileName.".php";
+    if(file_exists($filePath)){
+        ob_start();
+        extract($data);
+        include $filePath;
+        $output = ob_get_clean();
+    }
+    if($print){
+        print $output;
+    }
+}
---
+++ views/home.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+    <nav class = "navbar navbar-expand bg-dark navbar-dark">
+        <div class="container">
+            <a href = "#" class="navbar-brand">My Portfolio</a>
+            <ul class="navbar-nav mx-auto">
+                <li><a href="" class="nav-link">Home</a></li>
+                <li><a href="" class="nav-link">About</a></li>
+                <li><a href="" class="nav-link">Portfolio</a></li>
+                <li><a href="" class="nav-link">Contact</a></li>
+                <li><a href="" class="nav-link">Gallery</a></li>
+            </ul>
+            <ul class="navbar-nav">
+               <li><a href=""class="nav-link">Login</a></li> 
+               <li><a href=""class="nav-link">sign up</a></li>
+            </ul>
+        </div>        
+    </nav>
+    <div id= "slider" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1800">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/images/slider_1.jfif" alt="" srcset="" class="w-100" height="550"/>
+                <div class="carousel-caption">
+                    <h1>This is heading one</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        A amet dicta harum ipsa itaque iure nisi
+                        officiis quas quidem reiciendis.
+                    </p>
+                    <a href="" class="btn btn-success" >read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/slider_2.jfif" alt="" srcset="" class="w-100" height="550"/>
+                <div class="carousel-caption">
+                    <h1>This is heading one</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        A amet dicta harum ipsa itaque iure nisi
+                        officiis quas quidem reiciendis.
+                    </p>
+                    <a href="" class="btn btn-success" >read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/slider_3.jfif" alt="" srcset="" class="w-100" height="550"/>
+                <div class="carousel-caption">
+                    <h1>This is heading one</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        A amet dicta harum ipsa itaque iure nisi
+                        officiis quas quidem reiciendis.
+                    </p>
+                    <a href="" class="btn btn-success" >read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/slider_4.jfif" alt="" srcset="" class="w-100" height="550"/>
+                <div class="carousel-caption">
+                    <h1>This is heading one</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        A amet dicta harum ipsa itaque iure nisi
+                        officiis quas quidem reiciendis.
+                    </p>
+                    <a href="" class="btn btn-success" >read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/slider_5.jfif" alt="" srcset="" class="w-100" height="550"/>
+                <div class="carousel-caption">
+                    <h1>This is heading one</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        A amet dicta harum ipsa itaque iure nisi
+                        officiis quas quidem reiciendis.
+                    </p>
+                    <a href="" class="btn btn-success" >read more</a>
+                </div>
+            </div>
+        </div>
+        <a href="#slider" class="carousel-control-prev" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a href="#slider" class="carousel-control-next" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+
+
+    <section class="py-5 bg-info-subtle">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="assets/images/slider_5.jfif" alt="">
+                        <div class="card-body">
+                            <h4>This is heading content</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                            <a href=""class="btn btn-success">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                    <img src="assets/images/slider_6.jfif" alt="">
+                        <div class="card-body">
+                            <h4>This is heading content</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                            <a href=""class="btn btn-success">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                    <img src="assets/images/slider_7.jfif" alt="">
+                        <div class="card-body">
+                            <h4>This is heading content</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                            <a href=""class="btn btn-success">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <script src = "assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
